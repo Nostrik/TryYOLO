a4f71d09e6151380b0f19c590baa2b0ea49bee16
Nostrik <sairus.89@mail.ru>
2023-03-12 19:54:01 +0300

    start labels stufio cmd add

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")  # загрузите предварительно обученную модель YOLOv8n
-
-model.train(data="coco128.yaml")  # обучите модель
-model.val()  # оцените производительность модели на наборе проверки
-model.predict(source="https://ultralytics.com/images/bus.jpg")  # предсказать по изображению
-model.export(format="onnx")  # экспортируйте модель в формат ONNX
-
-# https://habr.com/ru/post/710016/
+# docker run -it --user root -p 8080:8080 -v `pwd`/mydata:/label-studio/data heartexlabs/label-studio:latest
