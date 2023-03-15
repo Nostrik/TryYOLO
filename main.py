123a4a2a3d7bd001b6b5baf3abc9f70503d63744
First_user <sairus.89@mail.ru>
2023-03-15 23:05:54 +0300

    yolo_first_start

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # docker run -it --user root -p 8080:8080 -v `pwd`/mydata:/label-studio/data heartexlabs/label-studio:latest
+from ultralytics import YOLO
+from loguru import logger
+import time
+
+model = YOLO("yolov8n.yaml")
+# model = YOLO("yolov8n.pt")
+
+
+def main():
+    results = model.train(data="coco128.yaml", epochs=3)
+
+
+if __name__ == '__main__':
+    logger.info(f"started main at {time.strftime('%X')}")
+    main()
+    logger.info(f"finished main at {time.strftime('%X')}")
+
