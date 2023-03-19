11418f0ebbac64cb38ad9ab22b945d661e5f1cce
Nostrik <sairus.89@mail.ru>
2023-03-19 22:11:40 +0300

    first start after train coco128

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ import time
 
 model = YOLO("yolov8n.yaml")
 # model = YOLO("yolov8n.pt")
+img1 = 'images/bb0684.jpg'
+img3 = 'images/000001.jpg'
+img4 = 'images/000002.jpg'
+img5 = 'images/000003.gif'
+img6 = 'images/000003.jpg'
+img7 = 'images/000004.jpeg'
+img8 = 'images/000004.jpg'
+img9 = 'images/000005.jpg'
+img0 = 'images/000006.jpg'
+img_list = [img0, img1, img3, img4, img5, img6, img7, img8, img9]
 
 
 def main():
     #  196: Cigar/Cigarette
-    results = model.train(data="coco128.yaml", epochs=1)
+    # results = model.train(data="coco128.yaml", epochs=1)
+    for img in img_list:
+        find = model(img)
 
 
 if __name__ == '__main__':
