f1621da36f9becc7b9864445baea15e054abb049
maksim <sairus.89@mail.ru>
2023-03-24 11:59:51 +0300

    update main2

--- main2.py
+++ main2.py
@@ -5,13 +5,30 @@ import time
 model = YOLO("yolov8n.yaml")
 # model = YOLO("yolov8n.pt")
 
+img1 = 'images/bb0684.jpg'
+img3 = 'images/000001.jpg'
+img4 = 'images/000002.jpg'
+img5 = 'images/000003.gif'
+img6 = 'images/000003.jpg'
+img7 = 'images/000004.jpeg'
+img8 = 'images/000004.jpg'
+img9 = 'images/000005.jpg'
+img0 = 'images/000006.jpg'
+img_list = [img0, img1, img3, img4, img5, img6, img7, img8, img9]
+
 
 def main():
     #  196: Cigar/Cigarette
+#    results = model.train(data="coco128.yaml", epochs=3000)
+#    for img in img_list:
+#        find = model(img)
     results = model.train(data="Objects365.yaml", epochs=1)
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     logger.info(f"started main at {time.strftime('%X')}")
     main()
+    end_time = time.time()
     logger.info(f"finished main at {time.strftime('%X')}")
+    logger.info(f"time spent is {end_time - start_time}")
