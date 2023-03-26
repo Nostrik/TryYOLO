5661dc0cb0b60ac780de2238f83a20782ce28052
Nostrik <sairus.89@mail.ru>
2023-03-26 13:28:47 +0300

    first find success

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 from loguru import logger
 import time
 
-model = YOLO("yolov8n.yaml")
+model = YOLO("runs/detect/train11/weights/best.pt")
 # model = YOLO("yolov8n.pt")
 img1 = 'images/bb0684.jpg'
 img3 = 'images/000001.jpg'
@@ -20,9 +20,9 @@ img_list = [img0, img1, img3, img4, img5, img6, img7, img8, img9]
 def main():
     #  196: Cigar/Cigarette
 #    results = model.train(data="coco128.yaml", epochs=3000)
-#     for img in img_list:
-#         find = model(img)
-    res = model('videos/videoplayback.mp4')
+    for img in img_list:
+        find = model(img)
+#     res = model('videos/videoplayback.mp4')
 
 
 if __name__ == '__main__':
