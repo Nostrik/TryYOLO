25d1393b1399042fba0d1447dd471f380f8a01f3
Nostrik <sairus.89@mail.ru>
2023-03-26 14:54:25 +0300

    train custom cigarette

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # docker run -it --user root -p 8080:8080 -v `pwd`/mydata:/label-studio/data heartexlabs/label-studio:latest
+# /usr/bin/python
 from ultralytics import YOLO
 from loguru import logger
 import time
 
-model = YOLO("runs/detect/train11/weights/best.pt")
+# model = YOLO("runs/detect/train11/weights/best.pt")
 # model = YOLO("yolov8n.pt")
+my_model = YOLO("yolov8n.yaml")
 img1 = 'images/bb0684.jpg'
 img3 = 'images/000001.jpg'
 img4 = 'images/000002.jpg'
@@ -18,10 +20,10 @@ img_list = [img0, img1, img3, img4, img5, img6, img7, img8, img9]
 
 
 def main():
-    #  196: Cigar/Cigarette
+    results = my_model.train(data="my_dataset_cigarette.yaml", epochs=1)
 #    results = model.train(data="coco128.yaml", epochs=3000)
-    for img in img_list:
-        find = model(img)
+#     for img in img_list:
+#         find = model(img)
 #     res = model('videos/videoplayback.mp4')
 
 
