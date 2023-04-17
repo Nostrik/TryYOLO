c17ceadcb9ff0f5011c13e5662cf27addf0af95d
First_user <sairus.89@mail.ru>
2023-04-17 12:44:17 +0300

    start beta train dataset

---
+++ test-cuda.py
@@ -0,0 +1,11 @@
+from cuda import cuda, nvrtc
+import numpy as np
+
+# Initialize CUDA Driver API
+err, = cuda.cuInit(0)
+
+# Retrieve handle for device 0
+err, cuDevice = cuda.cuDeviceGet(0)
+
+# Create context
+err, context = cuda.cuCtxCreate(0, cuDevice)
