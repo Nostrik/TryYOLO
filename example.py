d5aaf689eb36fab7f522e627a6d01ceac9e62d45
unknown <sairus.89@mail.com>
2023-06-26 23:22:57 +0300

    a bit more dev for prety stdout

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import time as tm
 from multiprocessing import Process, Manager, Lock, Barrier, Queue
+from loguru import logger
 
 
 def worker(process_number, info_container, queue):
@@ -13,6 +14,7 @@ def worker(process_number, info_container, queue):
 
 
 def printer(queue, num_processes, final_results, info_container):
+    cursor_up = lambda lines: '\x1b[{0}A'.format(lines)
     while True:
         output = ""
         for i in range(num_processes):
@@ -25,9 +27,9 @@ def printer(queue, num_processes, final_results, info_container):
                 output += " " * (len(f"Process {i}: 0    ")-1)
         results = [info_dict["value"] for info_dict in info_container]
         print(output, end='\r')
-        # if all(final_results):
-        #     break
+        logger.debug(value)
         tm.sleep(1)
+        print(cursor_up(6))
 
 
 if __name__ == "__main__":
@@ -44,6 +46,7 @@ if __name__ == "__main__":
         results_container = m.list([0] * num_processes)
         lock = Lock()
         queue = Queue()
+        print('\n' * num_processes)
         p_printer = Process(target=printer, args=(queue, num_processes, final_results, info_container))
         p_printer.start()
         for i in range(num_processes):
