305737e745b6ca0ad253c70708b65dc63d360ad3
unknown <sairus.89@mail.com>
2023-09-27 21:01:18 +0300

    dev vci

--- vci.py
+++ vci.py
@@ -2,8 +2,10 @@ import argparse
 import sys
 import docker
 from loguru import logger
+from docker.errors import DockerException
 
-docker_client = docker
+docker_client = docker.from_env()
+CONTAINER_NAME = "hello world"
 
 
 class MyParser(argparse.ArgumentParser):
@@ -13,10 +15,33 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 
-def main():
-    print("Start docker container ...")
-    pass
+def show_containers():
+    print("Show docker images..")
+    docker_client.images.list()
+
+
+def run_container(name_cont):
+    print(f"Start docker container ... {name_cont}")
+    docker_client.containers.run(name_cont)
+
+
+def main(args):
+    logger.debug(args)
+    if args.list_containers==True:
+        docker_client.containers.list()
+    else:
+        run_container(CONTAINER_NAME)
+
 
 
 if __name__ == "__main__":
-    main()
+    parser = MyParser(
+        prog="Custom Docker App",
+        description="custom app for flexible use docker engine"
+    )
+    parser.add_argument('-l', dest='list_containers', required=False, help='Show all docker images', action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+    try:
+        main(args)
+    except DockerException as error:
+        print(error)
